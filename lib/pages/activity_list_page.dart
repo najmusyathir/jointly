2436c9522a6153b17e0ad640f8187dd05a23eccf
najmusyathir <alsyathir@gmail.com>
2024-03-26 04:43:44 +0800

    Add: Upload activity_list_page.dart

---
+++ lib/pages/activity_list_page.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+void userDoubleTapped() {}
+
+void userLongPress() {}
+
+class ToDoList extends StatelessWidget {
+  const ToDoList({super.key, required this.name, required this.desc});
+
+  final String name;
+  final String desc;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        GestureDetector(
+          onTap: () {
+            Navigator.pushNamed(context, '/todoDetailsPage');
+          },
+          onDoubleTap: userDoubleTapped,
+          onLongPress: userLongPress,
+          child: Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(color: Colors.blueAccent)),
+            margin: const EdgeInsets.all(20),
+            padding: const EdgeInsets.all(10),
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  name,
+                  textAlign: TextAlign.left,
+                ),
+                Text(
+                  desc,
+                  textAlign: TextAlign.left,
+                )
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ActivityListPage extends StatelessWidget {
+  const ActivityListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: const <Widget>[
+          Column(
+            children: <Widget>[
+              Padding(padding: EdgeInsets.all(10)),
+              Text(
+                "Activity Page",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.w800,
+                  fontSize: 24,
+                ),
+              ),
+            ],
+          ),
+          ToDoList(name: 'Running', desc: 'Run for your life'),
+          ToDoList(name: 'Walking', desc: 'Walk and enjoy the views'),
+          ToDoList(name: 'Crawling', desc: 'Crawl if you want to survive'),
+          ToDoList(name: 'Sleep', desc: 'Sleep like you are already dead.'),
+        ],
+      ),
+    );
+  }
+}
